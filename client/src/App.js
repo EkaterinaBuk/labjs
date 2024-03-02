215c72cdf3d8ae58d0a7977e70e04ec3f1d6972e
Ekaterina <ebukaevva235@gmail.com>
2024-03-02 14:50:46 +0400

    cat

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import ex from './paskuda.jpg';
 function App() {
   return (
     <div className="App">
 <button>Нажми</button>
-<h1>bye bitch</h1>
+<h1>Hi;)</h1>
+    <img src={ex}/>
     </div>
   );
 }
